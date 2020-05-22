3ed501d8964845405e30cca7c6e5cbfcd18a73a5
Matthew Keggen <matthew.keggen@googlemail.com>
2020-05-22 15:50:58 +0100

    feature: NHibernate extensions

---
+++ Common/Source/BowlsResults.Common.Data/Extensions/NhibernateSetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using NHibernate.Proxy;
+
+namespace Com.BinaryBracket.BowlsResults.Common.Data.Extensions
+{
+    public static class NhibernateSetExtensions
+    {
+		public static T CastEntity<T>(this object entity) where T : class
+    	{
+    		var proxy = entity as INHibernateProxy;
+    		if (proxy != null)
+    		{
+    			return proxy.HibernateLazyInitializer.GetImplementation() as T;
+    		}
+    		else
+    		{
+    			return entity as T;
+    		}
+    	}
+    }
+}
---
+++ Common/Source/BowlsResults.Common.Domain/Extensions/NhibernateSetExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Com.BinaryBracket.BowlsResults.Common.Domain.Extensions
+{
+	public static class NhibernateSetExtensions
+	{
+		public static ReadOnlyCollection<T> ToReadOnlyCollection<T>(this IList<T> list)
+		{
+			return new ReadOnlyCollection<T>(list);
+		}
+
+		public static ReadOnlyCollection<T> ToReadOnlyCollection<T>(this ISet<T> set)
+		{
+			return new ReadOnlyCollection<T>(set.ToList());
+		}
+	}
+}
