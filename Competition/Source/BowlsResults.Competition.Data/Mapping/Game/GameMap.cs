994a9ad6961a0ed21cc9e4fd52e866733064be84
Matthew Keggen <matthew.keggen@googlemail.com>
2020-05-22 15:22:40 +0100

    feature: correct constructor mapping

--- Competition/Source/BowlsResults.Competition.Data/Mapping/Game/GameMap.cs
+++ Competition/Source/BowlsResults.Competition.Data/Mapping/Game/GameMap.cs
@@ -7,27 +7,27 @@ namespace Com.BinaryBracket.BowlsResults.Competition.Data.Mapping.Game
 	{
 		public GameMap()
 		{
-			Table("Game");
-			LazyLoad();
-			Map(x => x.GameVariationID).Column("GameVariationID");
-			Map(x => x.HomeResultTypeID).Column("HomeResultTypeID");
-			Map(x => x.AwayResultTypeID).Column("AwayResultTypeID");
-			Map(x => x.AssociationID).Column("AssociationID").Not.Nullable();
-			Map(x => x.SeasonID).Column("SeasonID").Not.Nullable();
-			Map(x => x.GameFormatID).Column("GameFormatID").Not.Nullable();
-			Map(x => x.GameCalculationEngineID).Column("GameCalculationEngineID").Not.Nullable();
-			Map(x => x.Date).Column("Date");
-			Map(x => x.VenueTypeID).Column("VenueTypeID");			
-			Map(x => x.HomeHandicap).Column("HomeHandicap");
-			Map(x => x.AwayHandicap).Column("AwayHandicap");
-			Map(x => x.HomeScore).Column("HomeScore").Not.Nullable();
-			Map(x => x.AwayScore).Column("AwayScore").Not.Nullable();
-			Map(x => x.HomeWalkover).Column("HomeWalkover").Not.Nullable();
-			Map(x => x.AwayWalkover).Column("AwayWalkover").Not.Nullable();
-			Map(x => x.Completed).Column("Completed").Not.Nullable();
-			Map(x => x.GameStatusID).Column("GameStatusID").Not.Nullable();
-			
-			References(x => x.Pitch).Column("PitchID").Not.Nullable();
+			this.Table("Game");
+			this.LazyLoad();
+			this.Map(x => x.GameVariationID).Column("GameVariationID");
+			this.Map(x => x.HomeResultTypeID).Column("HomeResultTypeID");
+			this.Map(x => x.AwayResultTypeID).Column("AwayResultTypeID");
+			this.Map(x => x.AssociationID).Column("AssociationID").Not.Nullable();
+			this.Map(x => x.SeasonID).Column("SeasonID").Not.Nullable();
+			this.Map(x => x.GameFormatID).Column("GameFormatID").Not.Nullable();
+			this.Map(x => x.GameCalculationEngineID).Column("GameCalculationEngineID").Not.Nullable();
+			this.Map(x => x.Date).Column("Date");
+			this.Map(x => x.VenueTypeID).Column("VenueTypeID");			
+			this.Map(x => x.HomeHandicap).Column("HomeHandicap");
+			this.Map(x => x.AwayHandicap).Column("AwayHandicap");
+			this.Map(x => x.HomeScore).Column("HomeScore").Not.Nullable();
+			this.Map(x => x.AwayScore).Column("AwayScore").Not.Nullable();
+			this.Map(x => x.HomeWalkover).Column("HomeWalkover").Not.Nullable();
+			this.Map(x => x.AwayWalkover).Column("AwayWalkover").Not.Nullable();
+			this.Map(x => x.Completed).Column("Completed").Not.Nullable();
+			this.Map(x => x.GameStatusID).Column("GameStatusID").Not.Nullable();
+
+			this.References(x => x.Pitch).Column("PitchID").Not.Nullable();
 
 			this.HasMany(x => x.Players)
 				.Not.KeyUpdate()
