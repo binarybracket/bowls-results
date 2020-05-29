ecbedf890f4f5f14f7f4026a910b55c65c4865cd
Matthew Keggen <matthew.keggen@googlemail.com>
2020-05-29 11:27:10 +0100

    feature: create round added to event

--- Competition/Source/BowlsResults.Competition.Domain/Entities/CompetitionEvent.cs
+++ Competition/Source/BowlsResults.Competition.Domain/Entities/CompetitionEvent.cs
@@ -1,4 +1,5 @@
 using Com.BinaryBracket.BowlsResults.Common.Domain.Entities;
+using Com.BinaryBracket.BowlsResults.Competition.Domain.Entities.Round;
 using Com.BinaryBracket.Core.Domain2.Entities;
 
 namespace Com.BinaryBracket.BowlsResults.Competition.Domain.Entities
@@ -16,13 +17,25 @@ namespace Com.BinaryBracket.BowlsResults.Competition.Domain.Entities
 		public virtual Season Season { get; set; }
 		public virtual int? SaveResultRuleSetID { get; set; }
 		public virtual int? DataInt1 { get; set; }
+
 		public virtual int? DataInt2 { get; set; }
 		//protected internal virtual IList<CompetitionRound> InternalCompetitionRounds { get; set; }
-		
+
 		//public virtual ReadOnlyCollection<CompetitionStage> Stages
-	//	{
-	//		get { return new ReadOnlyCollection<CompetitionStage>(this.InternalCompetitionRounds); }
-	//	}
-		
+		//	{
+		//		get { return new ReadOnlyCollection<CompetitionStage>(this.InternalCompetitionRounds); }
+		//	}
+
+		public virtual T CreateRound<T>(CompetitionRoundTypes competitionRoundTypeID, byte gameNumber) where T : CompetitionRound, new()
+		{
+			var round = new T();
+			round.Competition = this.Competition;
+			round.CompetitionEvent = this;
+			round.Season = this.Competition.Season;
+			round.CompetitionRoundTypeID = competitionRoundTypeID;
+			round.GameNumber = gameNumber;
+			round.CompetitionScopeID = this.Competition.CompetitionScopeID;
+			return round;
+		}
 	}
 }
