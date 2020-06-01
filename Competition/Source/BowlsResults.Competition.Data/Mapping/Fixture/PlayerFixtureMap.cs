c66e33ca449e280a117816afe1933152bbc39ca6
Matthew Keggen <matthew.keggen@googlemail.com>
2020-06-01 16:05:09 +0100

    fix: column for matches

--- Competition/Source/BowlsResults.Competition.Data/Mapping/Fixture/PlayerFixtureMap.cs
+++ Competition/Source/BowlsResults.Competition.Data/Mapping/Fixture/PlayerFixtureMap.cs
@@ -19,7 +19,7 @@ namespace Com.BinaryBracket.BowlsResults.Competition.Data.Mapping.Fixture
 			this.References(x => x.Entrant1).Column("Entrant1ID").Cascade.None();
 			this.References(x => x.Entrant2).Column("Entrant2ID").Cascade.None();
 			
-			this.HasMany(x => x.Matches).Fetch.Join().Cascade.None().Inverse().Access.CamelCaseField(Prefix.Underscore).AsSet();
+			this.HasMany(x => x.Matches).KeyColumn("FixtureID").Fetch.Join().Cascade.None().Inverse().Access.CamelCaseField(Prefix.Underscore).AsSet();
 			
 			this.DiscriminatorValue((int)CompetitionScopes.Player);
 		}
